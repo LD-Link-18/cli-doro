40f095e4d776b91baa6a1fcfd7b9a65848e30b00
LD-Link-18 <sametmertdik@outlook.com>
2023-04-01 22:06:35 +0300

    Added comments

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 #Classes
 class inputs_class():
     def __init__(self):
-        # Get the necessaryinputs
+        # Get the necessary inputs
         self.work_time = int(float(input("How long would you like a session to be? (in munites)\n"))*60)
         self.break_time = int(float(input("\nHow long would you like a break to be? (in munites)\n"))*60)
         self.reps = input("\nHow much repetitions do you want?\n")
@@ -20,21 +20,24 @@ def progress_bar(progress,total):
     print(f"\r|{bar}| %{percent:.2f}", end="\r")
 
 def timer(wt,bt,rps,rp,timer_type):
-
     if timer_type == "work":
+        #Record the starting time
         starting_time = time.time()
         print(f"Starting a session for {float(wt/60)} munites.")
         while True:
+            #Record the current time
             time_now = time.time()
             time_diff = time_now - starting_time
+            #Compare the difference between time_now and starting_time
+            #As long as the difference is less than the number user selected update the progress bar
             if time_diff < wt:
                 progress_bar(time_diff,wt)
             else:
                 print(f"\nFinished session {rp}/{rps}.")
                 print(f"Taking a break for {float(bt/60)} munites.")
-                break
-            
+                break       
     elif timer_type == "break":
+        #Same thing as the previous if statement
         starting_time = time.time()
         while True:
             time_now = time.time()
@@ -44,7 +47,6 @@ def timer(wt,bt,rps,rp,timer_type):
             else:
                 print(f"\nFinished break.")
                 break 
-
 
 def main_loop():
     print("Welcome to cli-doro.")
